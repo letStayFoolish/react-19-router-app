2ecda51bf3fc17992531b58ff7994bc2f810ea5a
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-12-16 18:58:57 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App.tsx";
+import "./index.css";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
